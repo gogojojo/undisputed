e7b0c3f1eb4f89268ec377f81691e53d07534875
gogojojo <joseph.gochua@gmail.com>
2015-04-16 17:32:19 -0700

    finished 2 column layout

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,35 @@
+$(function(){
+	var active = true
+
+	$('.middleleft').click(function(){
+		if(active == true){
+			$('.middleleft').css("width","25%");
+			$('.left').css("width","75%");
+			$('.middleright').css("width","0%");
+			$('.right').css("width","0%");
+			active = false;
+		} else if (active == false) {
+			$('.middleright').css("width","50%");
+			$('.right').css("width","0%");
+			$('.middleleft').css("width","50%");
+			$('.left').css("width","0%");
+			active = true;
+		}
+	});
+	$('.middleright').click(function(){
+		if(active == true) {
+			console.log('runit')
+			$('.middleright').css("width","25%");
+			$('.right').css("width","75%");
+			$('.middleleft').css("width","0%");
+			$('.left').css("width","0%");
+			active = false;
+		} else if(active == false) {
+			$('.middleright').css("width","50%");
+			$('.right').css("width","0%");
+			$('.middleleft').css("width","50%");
+			$('.left').css("width","0%");
+			active = true;
+		}
+	});
+})
